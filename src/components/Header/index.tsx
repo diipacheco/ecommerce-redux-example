00896e83e9246a099f488e53ee4948fba8c61de4
diipacheco <edilsonpacheco6@gmail.com>
2019-08-30 14:55:07 -0300

    Change the cart icon

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { MdSearch, MdShoppingCart } from 'react-icons/md';
+import { MdSearch, MdLocalGroceryStore } from 'react-icons/md';
 import { useDispatch } from 'react-redux';
 import { handleClick } from '../../store/Ducks/Cart/actions';
 import logo from '../../assets/logo.svg';
@@ -12,7 +12,7 @@ export default function Header() {
       <MdSearch size={35} color="#FFFF" />
       <img src={logo} alt="Logo" />
       <button type="button" onClick={() => dispatch(handleClick(true))}>
-        <MdShoppingCart size={35} color="#FFFF" />
+        <MdLocalGroceryStore size={35} color="#FFFF" />
       </button>
     </Container>
 );
